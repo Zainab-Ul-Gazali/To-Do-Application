f780f6db4d675a86ebba8b0094ca84aa725be53e
Zainab Ul Gazali <Zainabulgazali112@gmail.com>
2024-04-01 15:05:53 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect} from 'react'
+import {TodoProvider }from "./Context"
+import './App.css'
+import TodoForm from './Componenets/TodoForm'
+import TodoItem from './Componenets/TodoItem'
+
+function App() {
+  // State to manage todos
+  const [todos, setTodos] = useState([])
+   
+  // Function to add a new todo
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id:Date.now(), ...todo}, ...prev])
+  }
+   
+  // Function to update an existing todo
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+
+  }
+   
+  // Function to delete a todo
+  const deleteTodo = (id) =>{
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+   
+  // Function to toggle the completion status of a todo
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  }
+   
+  // Load todos from local storage when the component mounts
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+  
+  // Save todos to local storage whenever the todos state changes
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+
+  return (
+    <TodoProvider value={{todos, addTodo, deleteTodo, updateTodo, toggleComplete}}>
+    <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                        <TodoForm />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+
+                          </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
+  )
+}
+
+export default App
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# To-Do-Application
-A simple and intuitive Todo application built with React. Add, view, and delete tasks easily with this clean and minimalist interface.
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
